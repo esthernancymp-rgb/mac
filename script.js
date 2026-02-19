230b5930034fb3dbf2eb72a3d441f71a8d5ee27f
esthernancymp-rgb <esthernancymp@gmail.com>
2026-02-19 13:20:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,5 @@
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
-        header.classList.add("scrolled");
-    } else {
-        header.classList.remove("scrolled");
-    }
+document.querySelectorAll(".card").forEach(card => {
+card.addEventListener("click", () => {
+window.location = card.dataset.link;
 });
-
-const revealElements = document.querySelectorAll(".reveal");
-
-function revealOnScroll() {
-    const windowHeight = window.innerHeight;
-    revealElements.forEach(element => {
-        const elementTop = element.getBoundingClientRect().top;
-        if (elementTop < windowHeight - 100) {
-            element.classList.add("active");
-        }
-    });
-}
-
-window.addEventListener("scroll", revealOnScroll);
-
-const counters = document.querySelectorAll(".counter");
-
-counters.forEach(counter => {
-    counter.innerText = "0";
-    const updateCounter = () => {
-        const target = +counter.getAttribute("data-target");
-        const current = +counter.innerText;
-        const increment = target / 100;
-
-        if (current < target) {
-            counter.innerText = Math.ceil(current + increment);
-            setTimeout(updateCounter, 20);
-        } else {
-            counter.innerText = target;
-        }
-    };
-    updateCounter();
 });
-
